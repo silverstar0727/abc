e11cdb9ffe8c1a9c8ef8bf7221ad9deaacc6dc36
jeongmin <silverstar456@yonsei.ac.kr>
2022-11-26 10:09:21 +0000

    update: select by random select method

--- select_data.py
+++ select_data.py
@@ -13,12 +13,12 @@ os.makedirs(target_image_path, exist_ok=True)
 os.makedirs(target_label_path, exist_ok=True)
 
 data_list = os.listdir(image_path)
+data_list = random.sample(data_list, 30000)
 for data in tqdm(data_list):
-    if random.uniform(0, 1) > 0.04:
-        data_name = data.split(".")[0]
+    data_name = data.split(".")[0]
 
-        try:
-            shutil.copyfile(f"{image_path}/{data_name}.jpg", f"{target_image_path}/{data_name}.jpg")
-            shutil.copyfile(f"{label_path}/{data_name}.json", f"{target_label_path}/{data_name}.json")
-        except:
-            print(data_name)
+    try:
+        shutil.copyfile(f"{image_path}/{data_name}.jpg", f"{target_image_path}/{data_name}.jpg")
+        shutil.copyfile(f"{label_path}/{data_name}.json", f"{target_label_path}/{data_name}.json")
+    except:
+        print(data_name)
