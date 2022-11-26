6acd1a30c858b61e56d5180d18bbc4deb8ec4494
jeongmin <silverstar456@yonsei.ac.kr>
2022-11-26 10:50:59 +0000

    update: therm ir

--- select_data.py
+++ select_data.py
@@ -7,18 +7,44 @@ from tqdm import tqdm
 image_path = "/workspace/images"
 label_path = "/workspace/labels"
 
-target_image_path = "/workspace/yolov7-finetune/images"
-target_label_path = "/workspace/yolov7-finetune/labels"
-os.makedirs(target_image_path, exist_ok=True)
-os.makedirs(target_label_path, exist_ok=True)
+therm_image_path = "/workspace/yolov7-finetune/therm/images"
+therm_label_path = "/workspace/yolov7-finetune/therm/labels"
+ir_image_path = "/workspace/yolov7-finetune/ir/images"
+ir_label_path = "/workspace/yolov7-finetune/ir/labels"
+os.makedirs(therm_image_path, exist_ok=True)
+os.makedirs(therm_label_path, exist_ok=True)
+os.makedirs(ir_image_path, exist_ok=True)
+os.makedirs(ir_label_path, exist_ok=True)
 
 data_list = os.listdir(image_path)
-data_list = random.sample(data_list, 30000)
-for data in tqdm(data_list):
+random.shuffle(data_list)
+therm_list = []
+ir_list = []
+for idx, data in enumerate(data_list):
+    data_type = data.split("_")[2]
+    
+    if data_type == "THERM":
+        if len(therm_list) < 38000:
+            therm_list.append(data)
+    elif data_type == "IR":
+        if len(ir_list) < 38000:
+            ir_list.append(data)
+
+for data in tqdm(therm_list):
     data_name = data.split(".")[0]
 
     try:
-        shutil.copyfile(f"{image_path}/{data_name}.jpg", f"{target_image_path}/{data_name}.jpg")
-        shutil.copyfile(f"{label_path}/{data_name}.json", f"{target_label_path}/{data_name}.json")
+        shutil.copyfile(f"{image_path}/{data_name}.jpg", f"{therm_image_path}/{data_name}.jpg")
+        shutil.copyfile(f"{label_path}/{data_name}.json", f"{therm_label_path}/{data_name}.json")
+    except:
+        print(data_name)
+
+
+for data in tqdm(ir_list):
+    data_name = data.split(".")[0]
+
+    try:
+        shutil.copyfile(f"{image_path}/{data_name}.jpg", f"{ir_image_path}/{data_name}.jpg")
+        shutil.copyfile(f"{label_path}/{data_name}.json", f"{ir_label_path}/{data_name}.json")
     except:
         print(data_name)
